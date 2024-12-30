346a7202428cbdfaaf90a71ebab6800a40d271ea
olive <01ive@free.fr>
2024-12-30 18:35:02 +0100

    Add Kalman filter

--- compute_data.py
+++ compute_data.py
@@ -10,8 +10,11 @@ pd.options.plotting.backend = "plotly"
 
 process_accelerometer_data = False
 
-rotation_low_pass = True
-rotation_bias_processing = True
+rotation_low_pass = False
+acceleration_low_pass = False
+rotation_bias_processing = False
+
+Kalman_filter_processing = False
 
 ''' Functions '''
 def low_pass(data, filter):
@@ -41,6 +44,36 @@ def remove_bias(data):
     progress_bar.finish()
     return data
 
+def Kalman_filter(data):
+    # Initialisation des variables
+    theta_est = 0.0  # Angle estimé initial
+    P = 1.0  # Covariance initiale
+    Q = 0.001  # Bruit du processus (modèle dynamique)
+    R = 0.1   # Bruit de mesure (capteur)
+
+    accel_data = data['ACC rotation Y angle 2D']
+    gyro_data = data['Rot Y']
+    dt = data['Time delta']
+    data['Kalman filter Y'] = 0.0
+
+    # Filtre de Kalman
+    for i in data.index:
+        # Étape de prédiction
+        theta_pred = theta_est + gyro_data[i] * dt[i]
+        P_pred = P + Q
+        
+        # Calcul du gain de Kalman
+        K = P_pred / (P_pred + R)
+        
+        # Mise à jour de l'estimation
+        theta_est = theta_pred + K * (accel_data[i] - theta_pred)
+        P = (1 - K) * P_pred
+
+        data.loc[i, 'Kalman filter Y'] = theta_est
+
+    return data
+
+
 def compute_data(csv_file_name):
     logging.info("Start computing data from file: " + csv_file_name)
 
@@ -48,8 +81,13 @@ def compute_data(csv_file_name):
 
     # Calculate time starting to 0
     accelerometer_table['Time'] = accelerometer_table['Time'].apply(lambda x: x-accelerometer_table['Time'][0])
-    accelerometer_table['Time delta'] = accelerometer_table['Time'] - accelerometer_table['Time'].shift()
-    accelerometer_table['Time delta'] = accelerometer_table['Time delta'].shift(-1, fill_value=0)
+    accelerometer_table['Time delta'] = accelerometer_table['Time'] - accelerometer_table['Time'].shift(fill_value=0)
+
+    if acceleration_low_pass:
+        logging.info("Low pass filter calculation")
+        accelerometer_table['Acc X'] = low_pass(accelerometer_table['Acc X'], 0.99)
+        accelerometer_table['Acc Y'] = low_pass(accelerometer_table['Acc Y'], 0.99)
+        accelerometer_table['Acc Z'] = low_pass(accelerometer_table['Acc Z'], 0.99)
 
     # Low pass filter on Rotation sensor
     if rotation_low_pass:
@@ -62,6 +100,29 @@ def compute_data(csv_file_name):
     accelerometer_table['Rotation X'] = accelerometer_table['Rot X'] * accelerometer_table['Time delta']
     accelerometer_table['Rotation Y'] = accelerometer_table['Rot Y'] * accelerometer_table['Time delta']
     accelerometer_table['Rotation Z'] = accelerometer_table['Rot Z'] * accelerometer_table['Time delta']
+
+    # accelerometer_table['Rotation X'] = accelerometer_table['Rotation X'].apply(lambda x: 0 if np.abs(x) <= 0.001 else x)
+    # accelerometer_table['Rotation Y'] = accelerometer_table['Rotation Y'].apply(lambda x: 0 if np.abs(x) <= 0.001 else x)
+    # accelerometer_table['Rotation Z'] = accelerometer_table['Rotation Z'].apply(lambda x: 0 if np.abs(x) <= 0.001 else x)
+
+
+    # window = 10
+    # level = 0.001
+    # accelerometer_table['Rotation X filtered'] = accelerometer_table['Rotation X'].rolling(window=window, min_periods=1).mean()
+    # accelerometer_table['Rotation X filtered'] = np.abs(accelerometer_table['Rotation X filtered']) > level 
+    # accelerometer_table['Rotation X filtered'] = accelerometer_table['Rotation X'] * accelerometer_table['Rotation X filtered']
+    # accelerometer_table['Rotation X'] = accelerometer_table['Rotation X filtered'].cumsum()
+
+    # accelerometer_table['Rotation Y filtered'] = accelerometer_table['Rotation Y'].rolling(window=window, min_periods=1).mean()
+    # accelerometer_table['Rotation Y filtered'] = np.abs(accelerometer_table['Rotation Y filtered']) > level 
+    # accelerometer_table['Rotation Y filtered'] = accelerometer_table['Rotation Y'] * accelerometer_table['Rotation Y filtered']
+    # accelerometer_table['Rotation Y'] = accelerometer_table['Rotation Y filtered'].cumsum()
+
+    # accelerometer_table['Rotation Z filtered'] = accelerometer_table['Rotation Z'].rolling(window=window, min_periods=1).mean()
+    # accelerometer_table['Rotation Z filtered'] = np.abs(accelerometer_table['Rotation Z filtered']) > level 
+    # accelerometer_table['Rotation Z filtered'] = accelerometer_table['Rotation Z'] * accelerometer_table['Rotation Z filtered']
+    # accelerometer_table['Rotation Z'] = accelerometer_table['Rotation Z filtered'].cumsum()
+
     accelerometer_table['Rotation X'] = accelerometer_table['Rotation X'].cumsum()
     accelerometer_table['Rotation Y'] = accelerometer_table['Rotation Y'].cumsum()
     accelerometer_table['Rotation Z'] = accelerometer_table['Rotation Z'].cumsum()
@@ -69,14 +130,14 @@ def compute_data(csv_file_name):
     # Remove bias
     if rotation_bias_processing:
         logging.info("Removing bias")
-        accelerometer_table['Rotation X'] = remove_bias(accelerometer_table['Rotation X'])
-        accelerometer_table['Rotation Y'] = remove_bias(accelerometer_table['Rotation Y'])
-        accelerometer_table['Rotation Z'] = remove_bias(accelerometer_table['Rotation Z'])
+        remove_bias(accelerometer_table['Rotation X'])
+        remove_bias(accelerometer_table['Rotation Y'])
+        remove_bias(accelerometer_table['Rotation Z'])
 
     # Rotation angle in radians between -pi and pi
-    # accelerometer_table['Rotation X angle'] = accelerometer_table['Rotation X'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
-    # accelerometer_table['Rotation Y angle'] = accelerometer_table['Rotation Y'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
-    # accelerometer_table['Rotation Z angle'] = accelerometer_table['Rotation Z'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
+    accelerometer_table['Rotation X angle'] = accelerometer_table['Rotation X'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
+    accelerometer_table['Rotation Y angle'] = accelerometer_table['Rotation Y'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
+    accelerometer_table['Rotation Z angle'] = accelerometer_table['Rotation Z'].apply(lambda x: x if np.abs(x) <= np.pi else x - np.sign(x) * 2*np.pi)
 
     if process_accelerometer_data:
         # Calculate rotation angle from accelerator sensor
@@ -90,9 +151,13 @@ def compute_data(csv_file_name):
         accelerometer_table['ACC rotation Y angle 3D'] = np.arctan2(accelerometer_table['Acc Z'], np.sqrt(
                                             np.power(accelerometer_table['Acc Y'], 2) + 
                                             np.power(accelerometer_table['Acc X'], 2) )  )
-        accelerometer_table['ACC rotation Z angle 3D'] = np.arctan2(accelerometer_table['Acc Y'], np.sqrt(
+        accelerometer_table['ACC rotation Z angle 3D'] = np.arctan2(-accelerometer_table['Acc Y'], np.sqrt(
                                             np.power(accelerometer_table['Acc X'], 2) + 
                                             np.power(accelerometer_table['Acc Z'], 2) )  )
+
+    if Kalman_filter_processing:
+        logging.info("Kalman filter calculation")
+        accelerometer_table = Kalman_filter(accelerometer_table)
 
     # Use time col as index
     accelerometer_table = accelerometer_table.set_index('Time')
@@ -107,6 +172,7 @@ def compute_data(csv_file_name):
     json_file_name = csv_file_name.split('.')[0] + '.json'
     accelerometer_table.filter(['Rotation X', 'Rotation Y', 'Rotation Z'], axis=1).reset_index().to_json(json_file_name, orient='records', indent=2)
     logging.info("Json file generated: " + json_file_name)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
