ef169bfd999f54fc62e4ad1bfe139e7a24d37469
olive <01ive@free.fr>
2025-01-18 09:03:29 +0100

    Add hability to process all models in directory

--- model_to_js.py
+++ model_to_js.py
@@ -1,5 +1,18 @@
 import logging
 import sys
+import os
+
+def process_json_file(json_file):
+    logging.info(f"Reading model from {json_file}")
+    with open(json_file, 'r') as f:
+        model_json = f.read()
+    
+    text = "const model_data = " + model_json + ";"
+
+    js_file_name = json_file.split('.')[0] + '.js'
+    logging.info("Writing model to {}".format(js_file_name))
+    with open(js_file_name, 'w') as f:
+        f.write(text)
 
 
 if __name__ == "__main__":
@@ -7,13 +20,14 @@ if __name__ == "__main__":
     
     json_file_name = sys.argv[1]
 
-    logging.info(f"Reading model from {json_file_name}")
-    with open(json_file_name, 'r') as f:
-        model_json = f.read()
-    
-    text = "const model_data = " + model_json + ";"
-
-    js_file_name = json_file_name.split('.')[0] + '.js'
-    logging.info("Writing model to {}".format(js_file_name))
-    with open(js_file_name, 'w') as f:
-        f.write(text)
+    if not os.path.exists(json_file_name):
+        logging.error(f"File {json_file_name} does not exist.")
+        sys.exit(1)
+    elif os.path.isdir(json_file_name):
+        logging.info(f"Procesing all files in directory {json_file_name}")
+        for file in os.listdir(json_file_name):
+            if file.endswith('.json'):
+                process_json_file(os.path.join(json_file_name, file))
+    else:       
+        process_json_file(json_file_name)
+   
